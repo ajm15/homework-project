71202adb9563d5812d3b371b5d087b80c0f26c64
ajm15 <105832093+ajm15@users.noreply.github.com>
2022-05-21 15:29:41 -0400

    Update vars-clean.tf

--- vars-clean.tf
+++ vars-clean.tf
@@ -1,5 +1,6 @@
 variable "AWS_REGION" {
-    default = "us-east-1"
+    	default = "us-east-1"
+	type = string
 }
 variable "UBUNTU_AMI" {
     default = "ami-0076b30a832c25ac4"
@@ -8,10 +9,10 @@ variable "UBUNTU_AMI" {
 //redacted access key! This must be replaced before execution!
 variable "AWS_ACCESSKEY" {
 	default = "ABC123DEF456GHI789"
-    type = string
+	type = string
 }
 //redacted secret key! This must be replaced before execution!
-variable "AWS_SECRETKEY {
+variable "AWS_SECRETKEY" {
 	default = "0987654321abcdefghijklmnop"
 	type = string
 }
