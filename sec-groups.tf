620ee34a2ba99db4f65fa025421bbac7676f4fe5
ajm15 <105832093+ajm15@users.noreply.github.com>
2022-05-21 12:50:04 -0400

    Added comments to sec-groups.tf

--- sec-groups.tf
+++ sec-groups.tf
@@ -1,13 +1,15 @@
+//SG to be applied to public-facing ALB
 resource "aws_security_group" "public-alb-sg" {
     vpc_id = "${aws_vpc.redcanary-vpc.id}"
 
+    //allowing ingress from public internet over TCP/443
     ingress {
         from_port = 443
         to_port = 443
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
-
+    //allowing egress to TCP/443 with destination of private subnets
     egress {
         from_port = 443
         to_port = 443
@@ -20,9 +22,11 @@ resource "aws_security_group" "public-alb-sg" {
     }
 }
 
+//SG to be applied to internal application servers
 resource "aws_security_group" "private-app-sg" {
     vpc_id = "${aws_vpc.redcanary-vpc.id}"
 
+    //allowing ingress over TCP/443 from the public subnet
     ingress {
         from_port = 443
         to_port = 443
@@ -30,6 +34,8 @@ resource "aws_security_group" "private-app-sg" {
         cidr_blocks = ["10.0.0.0/23"]
     }
 
+    //allowing egress to TCP/443 with destination of anywhere
+    //this should be tightened up by specifying the numerical IP space of the externally accessed API services
     egress {
         from_port = 443
         to_port = 443
