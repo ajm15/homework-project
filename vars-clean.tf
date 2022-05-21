9512c6d7f3fd2d66a5c24912761ea5bef2e70038
ajm15 <105832093+ajm15@users.noreply.github.com>
2022-05-21 12:35:41 -0400

    Update vars-clean.tf
    Updated vars-clean.tf with comments about the need to supply valid AWS credentials.

--- vars-clean.tf
+++ vars-clean.tf
@@ -5,10 +5,12 @@ variable "UBUNTU_AMI" {
     default = "ami-0076b30a832c25ac4"
     type = string
 }
+//redacted access key! This must be replaced before execution!
 variable "AWS_ACCESSKEY" {
 	default = "ABC123DEF456GHI789"
     type = string
 }
+//redacted secret key! This must be replaced before execution!
 variable "AWS_SECRETKEY {
 	default = "0987654321abcdefghijklmnop"
 	type = string
